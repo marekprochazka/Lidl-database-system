4d49993f17ec9938c8832918b5c8ac2148ab971f
Marek Prochazka <m.prochazka2002@gmail.com>
2021-05-23 08:51:27 +0200

    small fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class DatabaseController:
         self.transactions = []
 
         self.SIMPLE_COMMANDS = (
-            '?', 'read', 'transaction', 'rollback', 'commit')
+            '?', 'read', 'transaction', 'rollback', 'commit', 'list', 'exit')
 
         self.commandswitch = {
             '?': self.__showHelp,
@@ -29,13 +29,13 @@ class DatabaseController:
         else:
             try:
                 self.__isValidCommand(command)
-                split_command = command.split(":")
+                split_command = command.split(':')
                 if len(split_command) == 1:
                     self.commandswitch[split_command[0]]()
                 else:
                     self.commandswitch[split_command[0]](split_command[1])
             except ValueError:
-                print("Invalid command")
+                print('Invalid command')
 
     def __showHelp(self):
         print(
@@ -50,37 +50,37 @@ class DatabaseController:
         print('exit          -> ends programme')
 
     def __readDatabase(self):
-        with open(self.database, "r") as db:
+        with open(self.database, 'r') as db:
             for id, value in enumerate(db.readlines()):
-                print(f"{id}: {value.strip()}")
+                print(f'{id}: {value.strip()}')
 
     def __writeLine(self, line: str):
-        with open(self.database, "a") as db:
-            db.write(line + "\n")
+        with open(self.database, 'a') as db:
+            db.write(line + '\n')
 
     def __removeLine(self, lineNumberString: str):
         try:
             lineNumber = int(lineNumberString)
             if lineNumber+1 > self.__getDatabaseLength():
                 raise ValueError
-            with open(self.database, "r") as db:
+            with open(self.database, 'r') as db:
                 dbList = db.readlines()
                 dbList.pop(lineNumber)
-            with open(self.database, "w") as db:
+            with open(self.database, 'w') as db:
                 db.writelines(dbList)
 
         except ValueError:
-            print("Invalid id")
+            print('Invalid id')
 
     def __startTransaction(self):
-        print("Transaction started")
+        print('Transaction started')
         self.isTransaction = True
 
     def __rollbackTransaction(self):
         if self.isTransaction:
             self.isTransaction = False
             self.transactions = []
-            print("Transaction rolledback")
+            print('Transaction rolledback')
         else:
             print('Transaction did not started')
 
@@ -95,32 +95,32 @@ class DatabaseController:
             print('Transaction did not started')
 
     def __addTransaction(self, command: str):
-        if command == "commit":
+        if command == 'commit':
             self.__commitTransaction()
-        elif command == "rollback":
+        elif command == 'rollback':
             self.__rollbackTransaction()
-        elif command == "list":
+        elif command == 'list':
             self.__transactionsList()
         else:
             try:
                 self.__isValidCommand(command)
                 self.transactions.append(command)
             except ValueError:
-                print("Invalid command")
+                print('Invalid command')
 
     def __transactionsList(self):
-        print("\n")
-        print("TRANSACTIONS LIST")
+        print('\n')
+        print('TRANSACTIONS LIST')
         for command in self.transactions:
             print(command)
-        print("\n")
+        print('\n')
 
     def __isValidCommand(self, command: str):
-        split_command = command.split(":")
+        split_command = command.split(':')
         if split_command[0] in self.commandswitch:
             if split_command[0] in self.SIMPLE_COMMANDS:
                 if len(split_command) == 1:
-                    if split_command[0] == "transaction" and self.isTransaction:
+                    if split_command[0] == 'transaction' and self.isTransaction:
                         raise ValueError
                     return
                 else:
@@ -129,21 +129,21 @@ class DatabaseController:
         raise ValueError
 
     def __getDatabaseLength(self):
-        with open(self.database, "r") as db:
+        with open(self.database, 'r') as db:
             return len(db.readlines())
 
     def __exit(self):
-        print("Goodbye")
+        print('Goodbye')
         sys.exit()
 
 
 def main():
-    controller = DatabaseController("db.txt")
-    print("Welcome to db programme!")
+    controller = DatabaseController('db.txt')
+    print('Welcome to db programme!')
     print("Type '?' to show commands.\n")
     while True:
         controller.handleInput(input())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
